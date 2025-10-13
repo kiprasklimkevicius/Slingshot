b2ee2ffa0be11ea8bb1ce0439e44b51b50388445
Kipras Klimkevičius <kipras.klim@gmail.com>
2025-10-13 15:03:44 +0200

    Revised Speed logic in Player Controller
    Speed is now Vector3 and latteral movement is adding speed along x-axis to the main speed vector.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,23 +3,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float initSpeed = 10;
-    public float speed;
-    public float lateralSpeed = 5;
+    public Vector3 speed;
+    public Vector3 lateralSpeed = new Vector3(5,0,0); // lateral speed
     public GameObject projectile;
     private float[] xPosProjectileSpawn = {-0.5f, 0.5f};
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        speed = initSpeed;
+        // Vector3.up is moving forward, because capsule is rotated 90 around x-axis;
+        speed = new Vector3(0, initSpeed, 0);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.up * speed * Time.deltaTime);
         LateralMovement();
+        transform.Translate(speed * Time.deltaTime);
         if (Input.GetKeyDown(KeyCode.Space))
         {
             ShootProjectile();
@@ -30,12 +31,10 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            //TODO: check behaviour of Time.deltaTime,
-            // I think its fine without this because its on discrete Input not Continuous pressing.
-            transform.Translate(Vector3.right * lateralSpeed);
+            speed += lateralSpeed;
         } if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.right * -lateralSpeed);
+            speed -= lateralSpeed;
         }
     }
     
